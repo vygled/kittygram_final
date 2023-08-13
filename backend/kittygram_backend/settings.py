4653b8a93d978af46c904703ff27728bd29590d7
Daniil Vygolov <vygled@yandex.ru>
2023-08-13 20:00:29 +0300

    1st review: hide SECRET_KEY & DEBUG variables

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,12 +1,15 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from dotenv import load_dotenv 
+
+load_dotenv() 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('SECRET_KEY').lower() == 'true'
 
 ALLOWED_HOSTS = ['158.160.73.88', '127.0.0.1', 'localhost', 'kittygram-v.serveirc.com']
 
